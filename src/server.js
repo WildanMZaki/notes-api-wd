ab496b259635154e093ff87d1475b59f0313cdb6
WildanMZaki <wildanmzaki7@gmail.com>
2023-06-22 09:16:35 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,33 @@
+// Server Configurations:
+require('dotenv').config();
+
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesService = require('./services/postgres/NoteService');
+const NotesValidator = require('./validators/notes');
+ 
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
